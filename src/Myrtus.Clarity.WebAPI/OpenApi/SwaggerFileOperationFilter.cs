907a8ff282986a574d2ab3a9a3dd3a484bda4b56
sercanio <104576153+sercanio@users.noreply.github.com>
2025-01-02 14:58:04 +0300

    fix: Add CMS module, update solution, and enhance Swagger
    - Added "modules/cms" submodule to `.gitmodules`.
    - Updated `Myrtus.Clarity.sln` to include new projects and configurations.
    - Added subproject commit reference in `cms` directory.
    - Updated `Myrtus.Clarity.WebAPI.csproj` with new package and project references.
    - Added `SwaggerFileOperationFilter` for file upload operations in Swagger.
    - Enhanced `Program.cs` to configure Swagger and dynamically load modules.
    - Added Azure Blob Storage settings to `appsettings.json`.
    - Cleaned up and added necessary `using` statements in `Program.cs`.
    - Updated rate limiting and authentication configuration in `Program.cs`.
    - Registered `SwaggerFileOperationFilter` as a transient service.

---
+++ src/Myrtus.Clarity.WebAPI/OpenApi/SwaggerFileOperationFilter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Myrtus.Clarity.WebAPI.OpenApi;
+
+internal sealed class SwaggerFileOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var consumesMultipart = context.MethodInfo
+            .GetCustomAttributes(true)
+            .OfType<ConsumesAttribute>()
+            .Any(attr => attr.ContentTypes.Contains("multipart/form-data"));
+
+        if (!consumesMultipart)
+            return;
+
+        var formFileParameters = context.MethodInfo.GetParameters()
+            .Where(p => p.ParameterType == typeof(IFormFile) || p.ParameterType == typeof(IEnumerable<IFormFile>))
+            .ToList();
+
+        if (!formFileParameters.Any())
+            return;
+
+        operation.Parameters.Clear();
+
+        operation.RequestBody = new OpenApiRequestBody
+        {
+            Content =
+        {
+            ["multipart/form-data"] = new OpenApiMediaType
+            {
+                Schema = new OpenApiSchema
+                {
+                    Type = "object",
+                    Properties = formFileParameters.ToDictionary(
+                        p => p.Name,
+                        p => new OpenApiSchema
+                        {
+                            Type = "string",
+                            Format = "binary"
+                        }),
+                    Required = new HashSet<string>(formFileParameters.Select(p => p.Name))
+                }
+            }
+        }
+        };
+    }
+}
